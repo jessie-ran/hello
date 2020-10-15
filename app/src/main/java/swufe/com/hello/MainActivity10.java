c6165f33906613ee9d0b7c0e9fdab3b474315de3
jessie-ran <jessie_ran@yeah.net>
2020-10-15 22:53:22 +0800

    2020/10/15

--- app/src/main/java/swufe/com/hello/MainActivity10.java
+++ app/src/main/java/swufe/com/hello/MainActivity10.java
@@ -3,12 +3,47 @@ package swufe.com.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 public class MainActivity10 extends AppCompatActivity {
+    String TAG="rateCalc";
+    float rate=0f;
+    TextView inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main11);
+        setContentView(R.layout.activity_main10);
+
+
+        String title =getIntent().getStringExtra("title");
+        rate=getIntent().getFloatExtra("rate",0f);
+        ((TextView)findViewById(R.id.title)).setText(title); //转成TextView对象，匿名对象
+        inp=findViewById(R.id.inp);
+        inp.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView show=(TextView)MainActivity10.this.findViewById(R.id.show);
+                if(s.length()>0){
+                    float val=Float.parseFloat(s.toString());
+                    show.setText(val+"RMB==>"+(rate*val));
+
+                }
+                else {
+                    show.setText("");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/swufe/com/hello/MainActivity9.java
+++ app/src/main/java/swufe/com/hello/MainActivity9.java
@@ -2,9 +2,11 @@ package swufe.com.hello;
 //这个是自定义的adapter
 import androidx.appcompat.app.AppCompatActivity;
 
+//现在要实现点击事件，和10结合起来，9是展示，10是点击到具体的之后可以进行计算和展示
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -35,24 +37,26 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-public class MainActivity9 extends  ListActivity implements Runnable {
+public class MainActivity9 extends  AppCompatActivity implements Runnable,AdapterView.OnItemClickListener {
     //自定义适配器Adapter
     private static final String TAG = "MainActivity9";
-    ListView listview = null;
+    ListView listview;
     Handler handler;
     //从网络上提取数据的时候才用得到
 
- private ArrayList<HashMap<String,String>> listItem;
+    private ArrayList<HashMap<String, String>> listItem;
     //存放文字、图片信息
     private SimpleAdapter listItemAdapter;//适配器
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main9);
-       // 调用
+       setContentView(R.layout.activity_main9);
+        // 调用
 
         //与页面布局文件中的listview关联
         listview = (ListView) findViewById(R.id.my_list);
+
         ArrayList<HashMap<String, String>> listItems = new ArrayList<HashMap<String, String>>();
         for (int i = 0; i < 10; i++) {
             HashMap<String, String> map = new HashMap<String, String>();
@@ -73,35 +77,39 @@ public class MainActivity9 extends  ListActivity implements Runnable {
             @Override
             public void handleMessage(Message msg) {
                 if (msg.what == 9) {
-                    List<HashMap<String,String>>list2=(List<HashMap<String, String>>)msg.obj;
-                    listItemAdapter=new SimpleAdapter(MainActivity9.this,list2, //listItem数据源
+                    List<HashMap<String, String>> list2 = (List<HashMap<String, String>>) msg.obj;
+                    listItemAdapter = new SimpleAdapter(MainActivity9.this, list2, //listItem数据源
                             R.layout.hangbuju1,
                             //ListItem的XML布局实现
-                            new String[]{"ItemTitle","ItemDetail"},
+                            new String[]{"ItemTitle", "ItemDetail"},
                             //数据的key
-                            new int[]{R.id.itemTitle,R.id.itemDetail}//布局里的id，k与id一一匹配
+                            new int[]{R.id.itemTitle, R.id.itemDetail}//布局里的id，k与id一一匹配
                     );
-                    setListAdapter(listItemAdapter);
+                    //listview.setAdapter(myAdapter);
+                    listview.setAdapter(listItemAdapter);
                 }
                 super.handleMessage(msg);
             }
         };
-
+        //这句话老是报错).setOnItemClickListener(this);
+        listview.setOnItemClickListener(this);
     }
+
+
     private String inputStream2String(InputStream inputStream) throws IOException {
-        final int bufferSize=1024;
-        final char[] buffer=new char[bufferSize];
-        final StringBuilder out=new StringBuilder();
-        Reader in =new InputStreamReader(inputStream,"gb2312");
-        while(true){
-            int rsz=in.read(buffer,0,buffer.length);
-            if(rsz<0)
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
                 break;
-            out.append(buffer,0,rsz);
+            out.append(buffer, 0, rsz);
         }
         return out.toString();
     }
-
+       @Override
     public void run() {
         URL url = null;
         String myre = null;
@@ -116,7 +124,7 @@ public class MainActivity9 extends  ListActivity implements Runnable {
             Log.i("thread", "run:html=" + html);
             //直接调用然后进行处理
             //      myre = useJsoup(html);
-            String str=html;
+            String str = html;
             Document doc = Jsoup.parse(str);
             Elements trs = doc.select("table").get(0).select("tr");
 //接下来写一个for循环得到一个string 数组
@@ -127,17 +135,17 @@ public class MainActivity9 extends  ListActivity implements Runnable {
             String huil = "ran";
             //  tablelen=trs.
             //一共是有28行Table表
-            for(int i=1;i<28;i++) {
+            for (int i = 1; i < 28; i++) {
                 Elements t1 = trs.get(i).select("td");
                 cou = t1.get(0).text();
 
                 huil = t1.get(5).text();
                 //直接汇率和间接汇率的转化
-                float bw=100f/Float.parseFloat(huil);
-                String er=bw+"";
-                HashMap<String, String> map =new HashMap<String, String>();
-                map.put("ItemTitle",cou);
-                map.put("ItemDetail",er);
+                float bw = 100f / Float.parseFloat(huil);
+                String er = bw + "";
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("ItemTitle", cou);
+                map.put("ItemDetail", er);
                 retList.add(map);
 
             }
@@ -154,4 +162,44 @@ public class MainActivity9 extends  ListActivity implements Runnable {
         handler.sendMessage(msg);
 
     }
+
+    //onItemClick获取数据
+    //可通过map或者view获取
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Object itemAtPosition = listview.getItemAtPosition(position);
+        //通过map获取数据
+     /*   HashMap<String,String> map = (HashMap<String, String>) itemAtPosition;
+        String titleStr = map.get("ItemTitle");
+        String detailStr = map.get("ItemDetail");
+        Log.i(TAG, "onItemClick: titleStr=" + titleStr);
+        Log.i(TAG, "onItemClick: detailStr=" + detailStr);
+        TextView title = (TextView) view.findViewById(R.id.itemTitle);
+        TextView detail = (TextView) view.findViewById(R.id.itemDetail);
+        String title2 = String.valueOf(title.getText());
+        String detail2 = String.valueOf(detail.getText());
+        Log.i(TAG, "onItemClick: title2=" + title2);
+        Log.i(TAG, "onItemClick: detail2=" + detail2);
+
+      */
+//通过view获取数据
+         TextView title=view.findViewById(R.id.itemTitle);
+        TextView detail=view.findViewById(R.id.itemDetail);
+        String title2=String.valueOf(title.getText());
+        String detail2=String.valueOf(detail.getText());
+
+
+
+    //打开新的界面，传递参数
+       Intent  intent=new Intent(this,MainActivity10.class);
+        intent.putExtra("title",title2);
+       // intent.putExtra("detail",detail2);
+        float mn=Float.parseFloat(detail2);
+        intent.putExtra("rate",Float.parseFloat(detail2));
+        startActivity(intent);
+
+    }
+
+
+
 }
